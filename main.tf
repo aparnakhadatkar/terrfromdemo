0a2919935d2274c1cebd29d55889dcc2beb4152e
aparnakhadatkar <aparnakhadatkar@gmail.com>
2025-10-05 15:39:01 +0530

    updates

--- main.tf
+++ main.tf
@@ -1,4 +1,97 @@
-resource azurerm_resource_group{
-    name       =   
-    location   = 
+provider azurerm{
+    features {}
+}
+
+resource azurerm_resource_group "main" {
+ name  =  "myrg05102025"
+ location  =  "eastus"
+}
+
+resource azurerm_storage_account "main" {
+ name                    = "str05102025"
+ resource_group          = azurerm_resource_group.main.name
+ location                = azurerm_resource_group.main.location
+ account_tier            = "standard"
+ account_replication_type = "LRS"
+
+}
+
+resource azurerm_virtual_network "main" {
+ name                    = "myvnet05203035"
+ resource_group          = azurerm_resource_group.main.name
+ location                = azurerm_resource_group.main.location
+ address_space           = ["10.0.0.0/16"]
+}
+
+resource azurerm_subnet "main" {
+ name                 = "mysubnet05203035"
+ resource_group       =  azurerm_resource_group.main.name
+ virtual_network_name = azurerm_virtual_network.main.name
+ address_prefix       = ["10.0.1.0/24]
+}  
+
+resource azurerm_public_ip "main" {
+ name               = "mypip05203035"
+ location           = azurerm_resource_group.main.location
+ resource_group     =  azurerm_resource_group.main.name
+ allocation_method  = "Daynamic"
+}
+
+## Network Interface
+resource azurerm_network_interface "main" {
+    name               = "mynic05203035"
+    location           = azurerm_resource_group.main.location
+    resource_group     = azurerm_resource_group.main.name
+
+    ip_configuration "main" {
+     name                         = "ipconfig1"
+     subnet_id                    = azurerm_subnet.main.id
+     private_ip_allocation_method = "Daynamic"
+     public_ip_address_id         = azurerm_public_ip.main.id      
+
+    }
+}
+
+## Virual Machine 
+
+resource azurerm_vitual_machine "main" {
+ name                  = "myvm05203035"
+ location              = azurerm_resource_group.main.location
+ resource_group        = azurerm_resource_group.main.name
+ size                  = "standard_B2s"
+ admin_username        = "azureuser"
+ admin_password        = "Password.1!!"
+ network_interface_ids = azurerm_network_interface.main.id
+
+ os_disk {
+    name                  = "win-os-disk"
+    caching               = "read-only"
+    storage_account_type  = "standard-LRS"
+ }
+
+ source_image_referance {
+   publisher            = "MicrosoftWindowsServer"
+   offer                = "WindowsServer"
+   Sku                  = "2029-Datacenter"
+   version              = "lastest"
+ }
+}
+
+## Kubernet Cluster
+
+resource azurerm_kubernetes_cluster {
+    name                  = "ask-demo"
+    location              = azurerm_resource_group.main.location
+    resource_group        = azurerm_resource_group.main.name
+    dns_prefix            = "aksdemo"
+
+    default_node_pool {
+        name          = "default"
+        node_count    = 1
+        vm_size       = "Standar_d2s_v3"
+    }
+
+    identity {
+        type  = "SystemAssigned"
+    }
 }
