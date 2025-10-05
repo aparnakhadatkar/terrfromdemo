dad7bfe69456b5fecfb3e7899347e86ceb7c2dca
aparnakhadatkar <99411739+aparnakhadatkar@users.noreply.github.com>
2025-10-05 16:38:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,3 @@
 provider "azurerm" {
   features = {}
-
-  # Optional: explicitly set environment variables if not already
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
 }
