26dedd95f74a4829c3b5fe0d4465bbde782d8886
aparnakhadatkar <99411739+aparnakhadatkar@users.noreply.github.com>
2025-10-05 19:56:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource azurerm_resource_group "main" {
- name  =  "myrg05102025"
+ name  =  "myrg051020251"
  location  =  "eastus"
 }
 
@@ -31,6 +31,7 @@ resource azurerm_public_ip "main" {
  location           = azurerm_resource_group.main.location
  resource_group_name = azurerm_resource_group.main.name
  allocation_method  = "Static"
+ sku                = "Standard"
 }
 
 ## Network Interface
