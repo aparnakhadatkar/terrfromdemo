29bc0d96914e43bd4446b51cde6325fb997f7514
aparnakhadatkar <99411739+aparnakhadatkar@users.noreply.github.com>
2025-10-05 18:43:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,7 @@
-provider azurerm {
-    features {}
+provider "azurerm" {
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  features        = {}
 }
